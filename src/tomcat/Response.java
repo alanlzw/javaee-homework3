7f3ef47a43c23108118bc357b1ca51069c204677
alanlzw <1473945615@qq.com>
2016-10-15 14:31:56 +0800

    Add files via upload

---
+++ src/tomcat/Response.java
@@ -0,0 +1,97 @@
+package tomcat;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+public class Response implements ServletResponse {
+    private PrintWriter writer;
+
+    public Response(OutputStream output) {
+        writer = new PrintWriter(output);
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return writer;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return "UTF-8";
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+
+    }
+
+    @Override
+    public void setContentLength(int i) {
+
+    }
+
+    @Override
+    public void setContentType(String s) {
+
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+	@Override
+	public void setContentLengthLong(long arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/tomcat/ServletProcessor.java
@@ -0,0 +1,44 @@
+package tomcat;
+
+import javax.servlet.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ServletProcessor {
+    public static void process(Request req, Response res) {
+        String urlPattern = req.getParameter("Uri");//"/Login"
+        String servletClass = WebXmlParser.getServletClass(urlPattern);
+        if (servletClass == null) {
+            String msg = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found";
+            try {
+                res.getWriter().print(msg);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                File dir=new File("src");
+                URL url=dir.toURL();
+                URL[] urls=new URL[]{url};
+                ClassLoader loader=new URLClassLoader(urls);
+                Class myClass = loader.loadClass(servletClass);
+                Servlet servlet = (Servlet) myClass.newInstance();
+                servlet.init(null);
+                servlet.service(req, res);
+                servlet.destroy();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }catch (ServletException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
